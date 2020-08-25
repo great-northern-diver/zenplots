27ba16b0357a307d64ab5ab5585a98966bf6c5bf
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2020-08-25 11:05:27 -0400

    Added to allow @useDynLib in roxygen

---
+++ R/zenplots.R
@@ -0,0 +1,19 @@
+#' zenplots: Zigzag Expanded Navigation Plots
+#'
+#' Zenplots, like pairs plots (scatterplot matrices), lay out a large
+#' number of one- and two-dimensional plots in an organized way.
+#' 
+#' Unlike pairs plots, zenplots can lay out a much larger number of
+#' plots by pursuing a zigzagging layout (following a zenpath) of 
+#' alternating one- and two-dimensional plots. 
+#' 
+#' The plots can be created by R's base graphics package, by the grid
+#' graphics package, or even made interactive (brushing, etc.) by using
+#' using the loon package.
+#' 
+#'
+#' @docType package
+#' @name zenplots
+#' @useDynLib zenplots
+NULL
+#> NULL
